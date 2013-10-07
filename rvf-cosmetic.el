a6c38dafbe719c07311bcb3afa7f5495fa7b93e3
Rafael Villarroel <rvf0068@gmail.com>
2013-10-06 21:08:18 -0500

    New file for cosmetic stuff

---
+++ rvf-cosmetic.el
@@ -0,0 +1,9 @@
+(setq frame-title-format "%b: %+%+ %f")
+
+;; to have the column number displayed
+(column-number-mode t)
+
+;; blink instead of beep when an error occurs
+(setq visible-bell t)
+
+(display-time)
