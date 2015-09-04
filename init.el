663ad08f72cf0716d8f9f1004c10ab4094e140c2
Rafael Villarroel <rvf0068@gmail.com>
2015-09-04 17:22:57 -0500

    Package initializing at the beginning of init.el

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (load "~/.emacs.d/rvf-elpa.el")
 
 (push "~/Dropbox/emacs/site-lisp/gnus/lisp" load-path)
@@ -152,4 +159,5 @@
 (if (equal system-name "lahp")
     (display-battery-mode)
   )
+
 
