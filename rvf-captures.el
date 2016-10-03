c089caa67e2c2bfbaa7361357e023be0ad2677fb
Rafael Villarroel <rvf0068@gmail.com>
2016-10-02 21:01:17 -0500

    Use org capture to get websites
    - Using python readability to get a site in an org buffer using pandoc. See
      https://lists.gnu.org/archive/html/emacs-orgmode/2016-09/msg00205.html. Had
      to install 'python pip' and run 'pip install readability-lxml'.
    - Capture a template, then use 'org-board' (see
      https://github.com/scallywag/org-board, but already in MELPA), to get
      a website using 'wget'.

--- rvf-captures.el
+++ rvf-captures.el
@@ -1,3 +1,96 @@
+;; code by Adam Porter, see https://lists.gnu.org/archive/html/emacs-orgmode/2016-09/msg00205.html
+(defun url-to-org-with-readability (url)
+  "Get page content of URL with python-readability, convert to
+Org with Pandoc, and display in buffer."
+
+  (let (title content new-buffer)
+
+    (with-temp-buffer
+      (unless (= 0 (call-process "python" nil '(t t) nil "-m" "readability.readability" "-u" url))
+        (error "Python readability-lxml script failed: %s" (buffer-string)))
+
+      ;; Get title
+      (goto-char (point-min))
+      (setq title (buffer-substring-no-properties (search-forward "Title:") (line-end-position)))
+
+      (unless (= 0 (call-process-region (point-min) (point-max) "pandoc" t t nil "--wrap=auto" "-f" "html" "-t" "org"))
+        (error "Pandoc failed."))
+      (setq content (buffer-substring (point-min) (buffer-end 1))))
+
+    ;; Make new buffer
+    (setq new-buffer (generate-new-buffer title))
+    (with-current-buffer new-buffer
+      (insert (concat "* [[" url "][" title "]]\n\n"))
+      (insert content)
+      (org-mode)
+      (goto-char (point-min))
+      (org-cycle)
+      (switch-to-buffer new-buffer))))
+
+;; derived from http://emacs.stackexchange.com/a/3990/29
+(defun my-yank-org-link (text)
+    (string-match org-bracket-link-regexp text)
+    (substring text (match-beginning 1) (match-end 1)))
+
+(defun my-yank-org-description (text)
+    (string-match org-bracket-link-regexp text)
+    (substring text (match-beginning 3) (match-end 3)))
+
+(defun read-url-with-org ()
+  "Call `url-to-org-with-readability' on URL in kill ring."
+  (interactive)
+  (url-to-org-with-readability (my-yank-org-link (first kill-ring))))
+
+(defun org-capture-web-page-with-readability (&optional url)
+  "Return string containing entire capture to be inserted in org-capture template."
+  (let ((url (my-yank-org-link (first kill-ring)))
+        ;; From org-insert-time-stamp
+        (timestamp (format-time-string (concat "[" (substring (cdr org-time-stamp-formats) 1 -1) "]")))
+        title title-linked content)
+
+    (with-temp-buffer
+      (unless (= 0 (call-process "python" nil '(t t) nil "-m" "readability.readability" "-u" url))
+        (error "Python readability-lxml script failed: %s" (buffer-string)))
+
+      ;; Get title
+      (goto-char (point-min))
+      (setq title (buffer-substring-no-properties (search-forward "Title:") (line-end-position)))
+      (setq title-linked (concat "[[" url "][" title "]]"))
+
+      (unless (= 0 (call-process-region (point-min) (point-max) "pandoc" t t nil "--no-wrap" "-f" "html" "-t" "org"))
+        (error "Pandoc failed."))
+
+      ;; Demote page headings in capture buffer to below the
+      ;; top-level Org heading and "Article" 2nd-level heading
+      (save-excursion
+        (goto-char (point-min))
+        (while (re-search-forward (rx bol (1+ "*") (1+ space)) nil t)
+          (beginning-of-line)
+          (insert "**")
+          (end-of-line)))
+
+      (goto-char (point-min))
+      (goto-line 2)
+      (setq content (s-trim (buffer-substring (point) (buffer-end 1))))
+
+      ;; Return capture for insertion
+      (concat title-linked " :website:\n\n" timestamp "\n\n** Article\n\n" content))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun org-capture-template-for-org-board ()
+  "Return string containing entire capture to be inserted in org-capture template."
+  (let ((url (my-yank-org-link (first kill-ring)))
+	(title (my-yank-org-description (first kill-ring)))
+        )
+
+      (concat title "\n"
+	      "  :PROPERTIES:\n"
+	      "  :URL: " url "\n"
+	      "  :WGET_OPTIONS: --recursive -l 1\n"
+	      "  :END:" )
+    ))
+
 (defun rvf/org-capture-protocol (keys description file)
   "Using protocols. Tags"
   `(,keys ,description entry
@@ -57,5 +150,10 @@
         ,(rvf/org-capture-misc
           "v" "Misc" "~/Dropbox/org/misc.org")  
         ,(rvf/org-capture-protocol
-          "z" "Misc from web" "~/Dropbox/org/misc.org")        
-      ))
+          "z" "Misc from web" "~/Dropbox/org/misc.org")
+	("w" "Use python-readability" entry
+	 (file "~/Dropbox/org/notes.org")
+	 "* %(org-capture-web-page-with-readability)")
+	("k" "Template for org-board" entry (file "~/Dropbox/org/board.org")
+	 "* %(org-capture-template-for-org-board)")
+	))
